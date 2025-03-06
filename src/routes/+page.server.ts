45debffcb37967da7a5bbaca21004c36cb3488ce
Lyla <mjuudev@gmail.com>
2025-03-05 22:07:06 -0500

    refactor: set default folder name

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,11 @@
 import climbingGymData from '$lib/data/climbing-gyms.json';
 
 export async function load() {
+	climbingGymData.gyms.map((gym) => {
+		gym.iconUrl = `gym-icon/${gym.iconUrl}`;
+		gym.imageUrl = `gym-preview/${gym.imageUrl}`;
+	});
+
 	return climbingGymData;
 }
 
