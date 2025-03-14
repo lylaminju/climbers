74e029851956542fe902a43bcfeb736e2ca02014
Lyla <mjuudev@gmail.com>
2025-03-13 23:24:01 -0400

    feat: add map view page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,24 @@
 <script lang="ts">
 	import { base } from '$app/paths';
+	import MapIcon from '$lib/icons/MapIcon.svelte';
 	import '../app.css';
+
 	let { children } = $props();
 </script>
 
-<header class="flex flex-row items-end gap-3 px-3 pt-3 pb-1 sm:px-4 sm:pt-4">
-	<h1 class="font-fugaz text-4xl font-bold text-gray-900 sm:text-5xl">
-		<a href="{base}/">RockClimberz</a>
-	</h1>
-	<p class="font-fugaz hidden text-xl text-slate-500 lg:block">Explore Climbing Gyms!</p>
+<header
+	class="flex flex-row items-end items-start justify-between gap-3 px-3 pt-3 pb-1 sm:px-4 sm:pt-4"
+>
+	<div>
+		<h1 class="font-fugaz text-4xl font-bold text-gray-900 sm:text-5xl">
+			<a href="{base}/">RockClimberz</a>
+		</h1>
+		<p class="font-fugaz text-xs text-slate-500 md:text-xl">Explore Climbing Gyms!</p>
+	</div>
 	<nav>
-		<!-- <a href="www.google.com">Join Google Map Group</a> -->
+		<a href="{base}/gmap-view">
+			<MapIcon styles="w-6 sm:w-8 stroke-slate-500 hover:stroke-black" />
+		</a>
 	</nav>
 </header>
 
