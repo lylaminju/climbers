c6ef36365467b7906e21858645da74fbf6c349cd
Lyla <mjuudev@gmail.com>
2025-03-14 14:12:37 -0400

    style: gmap-view

--- src/routes/gmap-view/+page.svelte
+++ src/routes/gmap-view/+page.svelte
@@ -1,17 +1,17 @@
-<section class="flex flex-col gap-3 md:grid md:grid-cols-2 md:gap-4">
+<section class="flex flex-col gap-3 md:grid md:grid-cols-2 md:gap-6">
 	<iframe
 		title="climbing gyms - ON"
 		src="https://www.google.com/maps/d/u/0/embed?mid=1xReA-poyVVXSenZ5e32WWMdVM3paNXE&ehbc=2E312F&noprof=1"
-		class="min-h-80 md:min-h-[480px] md:w-full"
+		class="border-md min-h-80 rounded-xl md:min-h-[480px] md:w-full"
 	></iframe>
 	<iframe
 		title="climbing gyms - BC"
 		src="https://www.google.com/maps/d/u/0/embed?mid=10tkuVcj5LGs9aM57AMkKMNk2_nwoQIo&ehbc=2E312F&noprof=1"
-		class="min-h-80 md:min-h-[480px] md:w-full"
+		class="border-md min-h-80 rounded-xl md:min-h-[480px] md:w-full"
 	></iframe>
 	<iframe
 		title="climbing gyms - QC"
 		src="https://www.google.com/maps/d/u/0/embed?mid=1lxG5RWlW_hI6KniO3NDNfRpRbHj6XwI&ehbc=2E312F&noprof=1"
-		class="min-h-80 md:min-h-[480px] md:w-full"
+		class="border-md min-h-80 rounded-xl md:min-h-[480px] md:w-full"
 	></iframe>
 </section>
