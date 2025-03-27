346ceec96368ce9d7018bc5556bc8431450f7b88
Lyla <mjuudev@gmail.com>
2025-03-27 14:04:18 -0400

    feat: improve routes UI

--- src/lib/icons/CurrentLocation.svelte
+++ src/lib/icons/CurrentLocation.svelte
@@ -1,14 +1,16 @@
+<script>
+	const { styles } = $props();
+</script>
+
 <svg
 	xmlns="http://www.w3.org/2000/svg"
-	width="18"
-	height="18"
 	viewBox="0 0 24 24"
 	fill="none"
 	stroke="currentColor"
 	stroke-width="2"
 	stroke-linecap="round"
 	stroke-linejoin="round"
-	class="lucide lucide-locate-fixed-icon lucide-locate-fixed"
+	class="lucide lucide-locate-fixed-icon lucide-locate-fixed {styles}"
 >
 	<line x1="2" x2="5" y1="12" y2="12" />
 	<line x1="19" x2="22" y1="12" y2="12" />
--- src/routes/RoutesDiv.svelte
+++ src/routes/RoutesDiv.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import { base } from '$app/paths';
 	import { TravelModes } from '$lib/enums/TravelModes';
+	import ChevronsRight from '$lib/icons/ChevronsRight.svelte';
 	import BicyclingIcon from '$lib/icons/commutes/BicyclingIcon.svelte';
 	import DrivingIcon from '$lib/icons/commutes/DrivingIcon.svelte';
 	import TransitIcon from '$lib/icons/commutes/TransitIcon.svelte';
 	import WalkingIcon from '$lib/icons/commutes/WalkingIcon.svelte';
 	import CurrentLocation from '$lib/icons/CurrentLocation.svelte';
 	import EllipsisVertical from '$lib/icons/EllipsisVertical.svelte';
+	import MapPin from '$lib/icons/MapPin.svelte';
 	import { Tooltip } from 'flowbite-svelte';
 
 	const { displayedGyms, isMobile, gymPlaceIds, searchRoutes } = $props();
@@ -16,8 +18,9 @@
 	id="routes"
 	class="flex h-fit w-full flex-row items-center gap-3 gap-y-2 text-sm text-slate-700 transition duration-300 sm:min-w-0 sm:text-base"
 >
+	<img src="{base}/google-map-icon.png" alt="Google Map Icon" class="hidden w-5 sm:inline" />
 	<div
-		class="grid w-fit grid-cols-2 divide-slate-300 rounded-lg border border-slate-300 sm:min-h-[38px] sm:grid-cols-4 sm:divide-x"
+		class="grid w-fit grid-cols-2 divide-slate-300 rounded-lg border border-slate-300 sm:grid-cols-4 sm:divide-x"
 	>
 		<button
 			onclick={() => searchRoutes(TravelModes.DRIVING)}
@@ -53,17 +56,22 @@
 	</div>
 	<div class="flex flex-col gap-y-0.5 sm:flex-row sm:items-center sm:gap-x-2">
 		<div class="flex flex-row items-center gap-1">
-			<CurrentLocation />
+			<CurrentLocation styles="w-4 sm:w-5" />
 			<span>Your location</span>
 		</div>
-		<span class="hidden sm:inline">» » »</span>
+
 		{#if isMobile}
 			<EllipsisVertical />
+		{:else}
+			<ChevronsRight styles="w-5" />
 		{/if}
+
 		<ul
 			class="no-scrollbar flex min-h-6 w-fit max-w-full flex-row items-center gap-1 overflow-x-scroll sm:min-w-0 sm:overflow-x-auto"
 		>
-			<img src="{base}/google-map-icon.png" alt="Google Map Icon" width="18" />
+			{#if isMobile || gymPlaceIds.length === 0}
+				<MapPin styles="w-4 sm:w-5" />
+			{/if}
 			{#if gymPlaceIds.length === 0}
 				<span class="text-slate-400">Add destinations</span>
 			{/if}
