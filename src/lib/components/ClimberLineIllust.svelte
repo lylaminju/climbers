f3251ddf6b2e3e265b4c49ca361ca21c018f2536
Lyla <mjuudev@gmail.com>
2025-05-01 22:07:31 -0400

    style: reduce opacity of climber illust

--- src/lib/components/ClimberLineIllust.svelte
+++ src/lib/components/ClimberLineIllust.svelte
@@ -1,5 +1,5 @@
 <img
 	src="/decor/climber-line-illust-man.png"
 	alt="Climber illustration"
-	class="absolute top-10 right-0 z-[-99] hidden h-[95%] max-w-none opacity-10 sm:block"
+	class="absolute top-10 right-0 z-[-99] hidden h-[95%] max-w-none opacity-7 sm:block"
 />
