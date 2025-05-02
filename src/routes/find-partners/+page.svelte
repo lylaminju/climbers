cf6b027087c2efd338a07f3a1dc595204a181398
Lyla <mjuudev@gmail.com>
2025-05-01 20:50:30 -0400

    feat: show attendees count in post list

--- src/routes/find-partners/+page.svelte
+++ src/routes/find-partners/+page.svelte
@@ -11,6 +11,7 @@
 		ClockOutline,
 		MapPinAltOutline,
 		PenOutline,
+		UserCircleOutline,
 		UserOutline,
 	} from 'flowbite-svelte-icons';
 	import { onMount } from 'svelte';
@@ -20,6 +21,7 @@
 			profile: { username: string };
 			gym: { name: string; city: string };
 			user_availability: Availability[];
+			acceptedJoinRequestsCount: number;
 		})[]
 	>([]);
 	let isLoading = $state(true);
@@ -30,7 +32,12 @@
 			const { data, error } = await supabase
 				.from('post')
 				.select(
-					'*, profile(username), gym(name, city), user_availability(date, start_time, end_time)',
+					`*, 
+					profile(username), 
+					gym(name, city), 
+					user_availability(date, start_time, end_time), 
+					join_requests:join_request!post_id(status)
+					`
 				)
 				.order('created_at', { ascending: false })
 				.is('deleted_at', null);
@@ -39,21 +46,20 @@
 				throw new Error('Failed to load posts.');
 			}
 
-			posts = data;
+			posts = data.map((post) => ({
+				...post,
+				acceptedJoinRequestsCount: post.join_requests.filter(
+					(request: { status: string }) => request.status === 'accepted'
+				).length,
+			}));
 		} catch (error) {
-			errorMessage =
-				error instanceof Error
-					? error.message
-					: 'Unable to load posts. Please try again.';
+			errorMessage = 'Error loading posts.';
 		} finally {
 			isLoading = false;
 		}
 	});
 
-	async function writePost() {
-		if (!$userStore) {
-			return;
-		}
+	function writePost() {
 		goto('/find-partners/write-post');
 	}
 </script>
@@ -92,24 +98,28 @@
 		{/if}
 	</div>
 
-	{#if errorMessage}
-		<p class="text-red-600">{errorMessage}</p>
-	{/if}
-
 	{#if isLoading}
 		<Spinner />
 	{/if}
 
-	{#if posts.length > 0}
+	{#if errorMessage}
+		<p class="text-red-600">{errorMessage}</p>
+	{:else if posts.length > 0}
 		<ol class="grid w-full grid-cols-1 gap-3 sm:w-3xl sm:grid-cols-2">
 			{#each posts as post}
 				<li>
 					<a
 						href="{base}/find-partners/{post.post_id}"
-						class="hover:bg-primary-50 flex h-fit flex-col gap-1 rounded-xl bg-white p-2 text-base sm:p-3 sm:text-xl"
+						class="hover:bg-primary-50 relative flex h-fit flex-col gap-1 rounded-xl bg-white p-2 text-base sm:p-3 sm:text-xl"
 					>
+						<span
+							class="absolute top-2 right-3 flex items-center gap-0.5 sm:top-3"
+						>
+							<UserOutline size="sm" />
+							<span class="text-sm">{post.acceptedJoinRequestsCount}</span>
+						</span>
 						<h2 class="flex items-center text-lg font-bold sm:text-xl">
-							<UserOutline class="mr-1 inline" />{post.profile.username}
+							<UserCircleOutline class="mr-1 inline" />{post.profile.username}
 						</h2>
 						<p class="flex items-center">
 							<MapPinAltOutline class="mr-1 inline" />{post.gym.name}
@@ -133,7 +143,7 @@
 				</li>
 			{/each}
 		</ol>
-	{:else if !isLoading}
+	{:else}
 		<p>No posts found.</p>
 	{/if}
 </section>
