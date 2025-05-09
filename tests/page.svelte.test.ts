be0c86afd8fb2f4c7478e48e61d55fc5edd42896
Lyla <mjuudev@gmail.com>
2025-05-09 18:01:09 -0400

    test: add test cases draft

--- tests/page.svelte.test.ts
+++ tests/page.svelte.test.ts
@@ -1,11 +1,39 @@
-import { describe, test, expect } from 'vitest';
 import '@testing-library/jest-dom/vitest';
 import { render, screen } from '@testing-library/svelte';
+import { describe, expect, test } from 'vitest';
 import Page from '../src/routes/+page.svelte';
 
-describe('/+page.svelte', () => {
+describe('Explore gyms', () => {
 	test('should render h1', () => {
 		render(Page);
 		expect(screen.getByRole('heading', { level: 1 })).toBeInTheDocument();
 	});
+	test.todo('render climbing gyms info cards');
+});
+
+describe('Climbing meetups', () => {
+	test.todo('render meetup posts');
+	test.todo('render [Write a post] button');
+});
+
+describe('Meetup Post', () => {
+	test.todo('render a post');
+	test.todo('render [Request to Join] button');
+
+	test.todo('request to join');
+});
+
+describe('Notifications', () => {
+	test.todo('render notifications');
+
+	test.todo('accept or decline request');
+});
+
+describe('Profile', () => {
+	test.todo('render Profile info');
+	test.todo('render My posts');
+	test.todo('render My requests');
+
+	test.todo('edit profile');
+	test.todo('delete profile');
 });
