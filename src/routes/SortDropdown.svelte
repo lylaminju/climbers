6a880d5d2b8c4794d93e2d2b489481797a9f8bb4
Lyla <mjuudev@gmail.com>
2026-02-01 13:17:59 -0500

    chore: remove sorting options - climbing area and price
    - adjust sorting button width

--- src/routes/SortDropdown.svelte
+++ src/routes/SortDropdown.svelte
@@ -1,5 +1,10 @@
 <script>
-	import { Button, Dropdown, DropdownDivider, DropdownItem } from 'flowbite-svelte';
+	import {
+		Button,
+		Dropdown,
+		DropdownDivider,
+		DropdownItem,
+	} from 'flowbite-svelte';
 	import { ChevronDownOutline } from 'flowbite-svelte-icons';
 
 	const sortGroups = [
@@ -11,24 +16,25 @@
 			{ value: 'nearest', label: 'Nearest Distance' },
 			{ value: 'farthest', label: 'Farthest Distance' },
 		],
-		[
-			{ value: 'smallest', label: 'Smallest Climbing Area' },
-			{ value: 'largest', label: 'Largest Climbing Area' },
-		],
-		[
-			{ value: 'cheapest', label: 'Price: Low to High' },
-			{ value: 'expensive', label: 'Price: High to Low' },
-		],
+		// [
+		// 	{ value: 'smallest', label: 'Smallest Climbing Area' },
+		// 	{ value: 'largest', label: 'Largest Climbing Area' },
+		// ],
+		// [
+		// 	{ value: 'cheapest', label: 'Price: Low to High' },
+		// 	{ value: 'expensive', label: 'Price: High to Low' },
+		// ],
 	];
 
 	const { selectedSortingOption = '', onSortChange = (value) => {} } = $props();
 </script>
 
 <Button
-	class="dropdown-btn flex w-full flex-row justify-between overflow-x-hidden sm:max-w-fit sm:min-w-[205px]"
+	class="dropdown-btn flex w-full flex-row justify-between overflow-x-hidden sm:w-fit"
 	aria-label="Sorting options"
 >
-	{sortGroups.flat().find((option) => option.value === selectedSortingOption)?.label}
+	{sortGroups.flat().find((option) => option.value === selectedSortingOption)
+		?.label}
 	<ChevronDownOutline class="ms-1 h-6 w-6 text-white sm:ms-2 dark:text-white" />
 </Button>
 <Dropdown>
@@ -38,7 +44,9 @@
 				onclick={() => {
 					onSortChange(option.value);
 				}}
-				class={selectedSortingOption === option.value ? 'bg-gray-100 dark:bg-gray-600' : ''}
+				class={selectedSortingOption === option.value
+					? 'bg-gray-100 dark:bg-gray-600'
+					: ''}
 			>
 				{option.label}
 			</DropdownItem>
